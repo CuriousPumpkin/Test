3b30910ad4523969aacc067ac6edad5e11bd5d27
Amartya Pattanayak <apattanayakofficial@gmail.com>
2026-06-25 20:21:05 +0530

    First draft

---
+++ paddle.py
@@ -0,0 +1,21 @@
+from Turtle import Turtle
+
+class paddle(Turtle):
+    def __init__(self, xcor, ycor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(xcor, ycor)
+
+    def go_up(self):
+
+        new_y = self.ycor() + 20
+        if new_y < 250:
+           self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        if new_y > -250:
+            self.goto(self.xcor(), new_y)
---
+++ pingpong.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.setup(width=800, height=600)
+
+
+
+
+
+
+
+screen.exitonclick()
