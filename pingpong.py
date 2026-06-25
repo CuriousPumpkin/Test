f20102990b0263294086bb5ffda9f3831609205a
Amartya Pattanayak <apattanayakofficial@gmail.com>
2026-06-26 00:49:32 +0530

    bug fixes v0.1

--- pingpong.py
+++ pingpong.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import paddle as pd
+from ball import Ball as PingPong
 
 screen = Screen()
 screen.bgcolor("black")
@@ -8,7 +9,15 @@ screen.setup(width=800, height=600)
 
 paddle1 = pd(350, 0)
 paddle2 = pd(-350, 0)
-# ball = PingPong()
+ball = PingPong()
+
+screen.listen()
+(screen.onkey(paddle1.go_up, "Up"))
+(screen.onkey(paddle1.go_down, "Down"))
+(screen.onkey(paddle2.go_down, "s"))
+(screen.onkey(paddle2.go_up, "w")) 
+
+
 
 
 
